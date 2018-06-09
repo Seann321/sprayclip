0378597dc8b351a6ac342b1f591bc50ad9a09c7c
Sean Eichenberg <officialseann@gmail.com>
2018-06-08 22:24:02 -0400

    Made it so Zack can see the screen.

--- src/sprayclip/Sprayclip.java
+++ src/sprayclip/Sprayclip.java
@@ -1,9 +1,23 @@
 package sprayclip;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 public class Sprayclip {
 
+    static Display display;
+    static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    static int width = (int) screenSize.getWidth();
+    static int height = (int) screenSize.getHeight();
+
     public static void main(String[] args) {
-        Display display = new Display("Stuff", 1280, 720);
+        if (width >= 1800 && height >= 1000) {
+            display = new Display("Stuff", 1600, 900);
+        } else if (width < 1600 && height < 900) {
+            display = new Display("Stuff", 1024, 576);
+        } else {
+            display = new Display("Stuff", 1280, 720);
+        }
     }
     
 }
