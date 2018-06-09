b9c5f185445903d4d75f5d85a62103b12d5b2b97
Sean Eichenberg <officialseann@gmail.com>
2018-06-08 22:18:30 -0400

    Added Display Class

--- src/sprayclip/Sprayclip.java
+++ src/sprayclip/Sprayclip.java
@@ -3,8 +3,7 @@ package sprayclip;
 public class Sprayclip {
 
     public static void main(String[] args) {
-        
-        
+        Display display = new Display("Stuff", 1280, 720);
     }
     
 }
